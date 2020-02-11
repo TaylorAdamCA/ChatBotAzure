160945009d97b66e0f85d4dfdd7415a825206563
Taylor Adam <tjadam1993@gmail.com>
2020-02-11 15:27:28 -0800

    initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=harrisontestbot-Web-Deploy.pubxml -p:Password=hobcbsvJnjfP5r0pjtpkZrRt2HicsRezRrzcldxRbkuqbRheX6aZNFJiDl6Y
+
